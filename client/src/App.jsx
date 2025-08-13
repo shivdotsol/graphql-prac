e5486713fa49a97321acfbec74133a23dca4173a
shivdotsol <defi.shiv@gmail.com>
2025-08-13 10:00:31 +0530

    apollo client added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+    query GetTodos {
+        getTodos {
+            title
+            completed
+            user {
+                name
+            }
+        }
+    }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const { data, loading, error } = useQuery(query);
+    if (loading) return <h1>Loading...</h1>;
+    if (error) return <h1>error</h1>;
+    return (
+        <>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                {data.getTodos.map((i) => (
+                    <div
+                        style={{ marginBottom: "30px", display: "flex" }}
+                        key={Math.random()}
+                    >
+                        {JSON.stringify(i)}
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new ApolloClient({
+    uri: "http://localhost:3000/graphql",
+    cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </StrictMode>
+);
